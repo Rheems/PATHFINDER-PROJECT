ef27b48be66e775bc78db5f2bbee7520716ddeab
Kareemah Ahmad Yusuf <akareema201@gmail.com>
2025-09-07 18:37:06 -0700

    Fix DashboardHome import path

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  build: {
+    outDir: "dist",
   },
-  server: {
-    port: 3000,
-    overlay: false,
-  },
+  // base: "/https://rheems.github.io/PATHFINDER-PROJECT/",
 });
