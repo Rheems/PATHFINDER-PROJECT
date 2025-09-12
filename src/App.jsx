b7f09e0c82170abfcddc8772c3927c5337bfecff
Kareemah Ahmad Yusuf <akareema201@gmail.com>
2025-09-12 08:46:10 -0700

    Fixed dependencies and ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
-import DashboardHome from "./pages/dashboard/DashboardHome.jsx";
+import DashboardHome from "./pages/Dashboard/DashboardHome.jsx";
 import Courses from "./pages/Courses";
 import Assignments from "./pages/Assignments";
 import Profile from "./pages/Profile";
